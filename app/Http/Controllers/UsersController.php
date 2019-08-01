d9a53871c0d7e50d6dbac98578963fad653bfbc8
jgchen <565572337@qq.com>
2019-08-01 05:36:24 +0000

    关注和粉丝列表页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -112,4 +112,18 @@ class UsersController extends Controller
         return redirect()->route('users.show',[$user]);
     }
 
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(20);
+        $title = $user->name.'||的关注';
+        return view('users.show_follow',compact('users','title'));
+    }
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(20);
+        $title = $user->name.'||的粉丝';
+        return view('users.show_follow',compact('users','title'));
+
+    }
+
 }
